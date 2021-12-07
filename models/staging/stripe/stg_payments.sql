3bf79c672723a0f202f86683b2c520bfd1016f25
Johannes Lochter <johannes.lochter@cherre.com>
2021-12-07 21:20:45 +0000

    added macro to convert cents to dollars

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,15 +1,8 @@
-with payments as (
-
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status,
-        amount / 100 as amount, -- converted to dollars
-        created as created_at
-
-    from {{ source('stripe', 'payment') }}
-
-)
-
-select * from payments
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+from {{ source('stripe','payment') }}
