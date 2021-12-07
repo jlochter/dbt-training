8bb459b43b4f681e2f9d3dd70a89f06f2b9c6c74
Johannes Lochter <johannes.lochter@cherre.com>
2021-12-07 17:51:55 +0000

    using sources

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,15 @@
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount / 100 as amount, -- converted to dollars
+        created as created_at
+
+    from {{ source('stripe', 'payment') }}
+
+)
+
+select * from payments
